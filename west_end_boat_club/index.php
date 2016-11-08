af8e108f14c18a0bc416ccb1d98c6d5d8f45333c
CordellConner <cdc10@students.ptcollege.edu>
2016-11-08 14:21:18 -0500

    Site Start
    Basic folder structure, with blank index.php, style, reset, and script files.

---
+++ west_end_boat_club/index.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+?><!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>West End Boat Club</title>
+<meta name="viewport" content="initial-scale=1.0,width=device-width,maximum-scale=1,minimum-scale=1,user-scalable=no" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="keywords" content="West, End, Boat, Club, DPI, Design, Pennsylvania, Innovations" />
+<meta name="description" content="Philly's local boat club right on the West side of the river." />
+<!--[if IE]>   <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>  <![endif]-->
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<script src="js/script.js"></script>
+</head>
+
+<body><!---------------------------------------------------------- Begin Body ----------------------------------------------------------------------------->
+
+
+
+
+
+
+</body><!---------------------------------------------------------- End Body ------------------------------------------------------------------------------>
+</html>
